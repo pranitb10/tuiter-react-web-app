338f72a82271f0cacfb80f7ee32d60042d27c8df
Pranit Brahmbhatt <pranit@esamudaay.com>
2023-06-05 22:08:14 -0400

    updating states of the by adding reducers

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -7,9 +7,17 @@ import BookmarksScreen from "./bookmarks/bookmarks-screen";
 import ProfileScreen from "./profile/profile-screen";
 import WhoToFollowList from "./who-to-follow-list/index";
 import TuitSummaryList from "./tuit-summary-list/index";
+import whoReducer from "./reducers/who-reducer";
+import tuitsReducer from "./reducers/tuits-reducer";
+import { configureStore } from '@reduxjs/toolkit';
+import {Provider} from "react-redux";
+
+const store = configureStore(
+  {reducer: {who: whoReducer, tuits: tuitsReducer}});
 
 function Tuiter() {
  return (
+  <Provider store={store}>
    <div>
      <Nav />
      <div className="row">
@@ -29,6 +37,7 @@ function Tuiter() {
       </div>
      </div>
    </div>
+  </Provider>
  );
 }
 export default Tuiter;
