7ba0a833c1815d4155e362a36b3fb79b25fa959d
Pranit Brahmbhatt <pranit@esamudaay.com>
2023-06-06 21:26:44 -0400

    assignment completed. Also updated dependencies for react-icons

--- src/tuiter/home/home-screen.js
+++ src/tuiter/home/home-screen.js
@@ -1,4 +1,14 @@
-function HomeScreen() {
-    return <h1>Home</h1>;
+import React from "react";
+import TuitsList from "../tuits/TuitList";
+import WhatsHappening from "../whatsHappening";
+
+const HomeScreen = () => {
+    return(
+        <>
+            <h4>Home</h4>
+            <WhatsHappening/>
+            <TuitsList/>
+        </>
+    );
 }
 export default HomeScreen;
